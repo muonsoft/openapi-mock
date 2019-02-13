ad84ffab0f55eabbb95047c05164805fb1d51174
Igor Lazarev <strider2038@yandex.ru>
2019-02-13 08:12:30 +0300

    invalid value generator

---
+++ src/Mock/Generation/Value/InvalidValueGenerator.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * This file is part of Swagger Mock.
+ *
+ * (c) Igor Lazarev <strider2038@yandex.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Mock\Generation\Value;
+
+use App\Mock\Parameters\Schema\Type\InvalidType;
+use App\Mock\Parameters\Schema\Type\TypeInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * @author Igor Lazarev <strider2038@yandex.ru>
+ */
+class InvalidValueGenerator implements ValueGeneratorInterface
+{
+    /** @var LoggerInterface */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function generateValue(TypeInterface $type)
+    {
+        \assert($type instanceof InvalidType);
+
+        $this->logger->warning(
+            sprintf(
+                'Message generation was ignored due to invalid type with error: "%s".',
+                $type->getError()
+            )
+        );
+
+        return null;
+    }
+}
---
+++ tests/Unit/Mock/Generation/Value/InvalidValueGeneratorTest.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * This file is part of Swagger Mock.
+ *
+ * (c) Igor Lazarev <strider2038@yandex.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Unit\Mock\Generation\Value;
+
+use App\Mock\Generation\Value\InvalidValueGenerator;
+use App\Mock\Parameters\Schema\Type\InvalidType;
+use App\Tests\Utility\TestCase\LoggerTestCaseTrait;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Igor Lazarev <strider2038@yandex.ru>
+ */
+class InvalidValueGeneratorTest extends TestCase
+{
+    use LoggerTestCaseTrait;
+
+    private const ERROR = 'error';
+
+    protected function setUp(): void
+    {
+        $this->setUpLogger();
+    }
+
+    /** @test */
+    public function generateValue_invalidType_nullReturnedAndErrorLogged(): void
+    {
+        $generator = new InvalidValueGenerator($this->logger);
+        $type = new InvalidType(self::ERROR);
+
+        $value = $generator->generateValue($type);
+
+        $this->assertNull($value);
+        $this->assertLogger_warning_wasCalledOnce('Message generation was ignored due to invalid type with error: "error".');
+    }
+}
