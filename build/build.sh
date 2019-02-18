2db032bb33a52d734c478ac64da7618adef3e847
Igor Lazarev <strider2038@yandex.ru>
2019-02-18 21:53:32 +0300

    final travis config for release

--- build/build.sh
+++ build/build.sh
@@ -4,14 +4,16 @@ image_name="swaggermock/swagger-mock"
 
 if [ "$TRAVIS_TAG" != "" ]; then
     version="${TRAVIS_TAG:1}"
-    image_name="$image_name:$version"
 fi
 
-echo "Builder docker image $image_name..."
+echo "Builder docker image ${image_name}..."
 
-docker build --pull --tag "$image_name" .
+docker build --pull --tag "${image_name}" .
 if [ "${?}" != "0" ]; then
     exit 1;
 fi
+
+docker tag "${image_name}" "${image_name}:latest"
+docker tag "${image_name}" "${image_name}:${version}"
 
 docker images
--- build/deploy.sh
+++ build/deploy.sh
@@ -4,13 +4,17 @@ image_name="swaggermock/swagger-mock"
 
 if [ "$TRAVIS_TAG" != "" ]; then
     version="${TRAVIS_TAG:1}"
-    image_name="$image_name:$version"
 fi
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-echo "Deploying image $image_name to DockerHub..."
+echo "Deploying image ${image_name} to DockerHub..."
 
-docker push "$image_name"
+docker push "${image_name}"
+if [ "${?}" != "0" ]; then
+    exit 1;
+fi
+
+docker push "${image_name}:$version"
 if [ "${?}" != "0" ]; then
     exit 1;
 fi
