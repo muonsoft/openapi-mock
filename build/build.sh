6998824b63ef6cefdc14cc9252b61ae73b47ea67
Igor Lazarev <strider2038@yandex.ru>
2019-02-18 21:26:23 +0300

    travis config for tag release

--- build/build.sh
+++ build/build.sh
@@ -6,10 +6,9 @@ if [ "$TRAVIS_TAG" != "" ]; then
     image_name="$image_name:$TRAVIS_TAG"
 fi
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-echo "Deploying image $image_name to DockerHub..."
-
-docker push "$image_name"
+docker build --pull --tag "swaggermock/swagger-mock" .
 if [ "${?}" != "0" ]; then
     exit 1;
 fi
+
+docker images
