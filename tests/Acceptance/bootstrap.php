ff281900a384e3e1beab44faa86c6b956bad2c81
Igor Lazarev <strider2038@yandex.ru>
2020-05-06 12:02:50 +0300

    php code removed

--- tests/Acceptance/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// The check is to ensure we don't use .env in production
-if (!isset($_SERVER['APP_ENV'])) {
-    throw new \RuntimeException('APP_ENV environment variable is not defined.');
-}
