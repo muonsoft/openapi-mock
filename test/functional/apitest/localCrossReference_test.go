5042ecab073e35d6be0c35728cca35e9eb5ac01e
Igor Lazarev <strider2038@yandex.ru>
2020-05-11 17:15:49 +0300

    test: local cross reference

---
+++ test/functional/apitest/localCrossReference_test.go
@@ -0,0 +1,23 @@
+package apitest
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"swagger-mock/internal/di/config"
+	"swagger-mock/pkg/jsonassert"
+)
+
+func (suite *APISuite) TestLocalCrossReference_LocalPathReference_ReferenceResolved() {
+	recorder := httptest.NewRecorder()
+	handler := suite.createOpenAPIHandler(config.Configuration{
+		SpecificationURL: "local-cross-reference.yaml",
+	})
+
+	request, _ := http.NewRequest("GET", "/entities", nil)
+	handler.ServeHTTP(recorder, request)
+
+	suite.Equal(http.StatusOK, recorder.Code)
+	suite.Equal("application/json", recorder.Header().Get("Content-Type"))
+	json := jsonassert.MustParse(suite.T(), recorder.Body.Bytes())
+	json.AssertNodeEqualToTheString("$[0].key", "value")
+}
