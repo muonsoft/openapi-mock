3c7a8b20c6f96c7cbe890a810a47b67cbcfe26fe
Igor Lazarev <strider2038@yandex.ru>
2020-05-13 20:34:56 +0300

    minor: improved ci linter

--- internal/mock/generator/randomGenerator.go
+++ internal/mock/generator/randomGenerator.go
@@ -1,6 +1,6 @@
 package generator
 
-// Contract for random generator compatible with rand.Rand from math/rand package
+// Contract for random generator compatible with rand.Rand from math/rand package.
 type randomGenerator interface {
 	Float64() float64
 	Intn(n int) int
