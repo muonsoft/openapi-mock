17a9c9b9d0939fffd499d6500b78a42a9db2b5df
Igor Lazarev <strider2038@yandex.ru>
2020-05-16 17:52:37 +0300

    test: server urls in routing

---
+++ test/functional/apitest/ServerURLsInRouting_test.go
@@ -0,0 +1,94 @@
+package apitest
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"swagger-mock/internal/di/config"
+	"swagger-mock/pkg/jsonassert"
+	"testing"
+)
+
+func (suite *APISuite) TestServerURLsInRouting_SendingGETToEndpointsWithGlobalAndPathAndEndpointServers_200StatusAndExpectedContent() {
+	tests := []struct {
+		route string
+	}{
+		{"/global/base/path/endpoint"},
+		{"/another-global/base/path/endpoint"},
+		// Server override at path and endpoint level is not supported yet because it is not
+		// supported in https://github.com/getkin/kin-openapi
+		// {"/local/base/path/second-endpoint"},
+		// {"/another-local/base/path/second-endpoint"},
+		// {"/endpoint/base/path/third-endpoint"},
+		// {"/another-endpoint/base/path/third-endpoint"},
+	}
+	for _, test := range tests {
+		suite.T().Run(test.route, func(t *testing.T) {
+			recorder := httptest.NewRecorder()
+			handler := suite.createOpenAPIHandler(config.Configuration{
+				SpecificationURL: "ServerURLsInRouting.yaml",
+			})
+
+			request, _ := http.NewRequest("GET", test.route, nil)
+			handler.ServeHTTP(recorder, request)
+
+			assert.Equal(t, http.StatusOK, recorder.Code)
+			assert.Equal(t, "application/json; charset=utf-8", recorder.Header().Get("Content-Type"))
+			json, err := jsonassert.Parse(t, recorder.Body.Bytes())
+			assert.NoError(t, err)
+			if err == nil {
+				json.AssertNodeEqualToTheString("$.key", "value")
+			}
+		})
+	}
+}
+
+func (suite *APISuite) TestServerURLsInRouting_SendingPOSTToNonExistentNotOverriddenEndpoints_404Status() {
+	tests := []struct {
+		route string
+	}{
+		{"/endpoint/base/path/third-endpoint"},
+		{"/another-endpoint/base/path/third-endpoint"},
+	}
+	for _, test := range tests {
+		suite.T().Run(test.route, func(t *testing.T) {
+			recorder := httptest.NewRecorder()
+			handler := suite.createOpenAPIHandler(config.Configuration{
+				SpecificationURL: "ServerURLsInRouting.yaml",
+			})
+
+			request, _ := http.NewRequest("POST", test.route, nil)
+			handler.ServeHTTP(recorder, request)
+
+			assert.Equal(t, http.StatusNotFound, recorder.Code)
+		})
+	}
+}
+
+func (suite *APISuite) TestServerURLsInRouting_SendingPOSTToExistentNotOverriddenEndpoints_200Status() {
+	tests := []struct {
+		route string
+	}{
+		{"/global/base/path/third-endpoint"},
+		{"/another-global/base/path/third-endpoint"},
+	}
+	for _, test := range tests {
+		suite.T().Run(test.route, func(t *testing.T) {
+			recorder := httptest.NewRecorder()
+			handler := suite.createOpenAPIHandler(config.Configuration{
+				SpecificationURL: "ServerURLsInRouting.yaml",
+			})
+
+			request, _ := http.NewRequest("POST", test.route, nil)
+			handler.ServeHTTP(recorder, request)
+
+			assert.Equal(t, http.StatusOK, recorder.Code)
+			assert.Equal(t, "application/json; charset=utf-8", recorder.Header().Get("Content-Type"))
+			json, err := jsonassert.Parse(t, recorder.Body.Bytes())
+			assert.NoError(t, err)
+			if err == nil {
+				json.AssertNodeEqualToTheString("$.key", "value")
+			}
+		})
+	}
+}
