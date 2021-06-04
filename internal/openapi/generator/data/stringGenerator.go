5179110e9fc15f99a5179c6b63b4fccf52aa918d
Xiaolin Wang <exgphe@icloud.com>
2021-06-04 23:33:54 +0800

    Use reggen instead of faker.Regexify to generate string value by pattern better

--- internal/openapi/generator/data/stringGenerator.go
+++ internal/openapi/generator/data/stringGenerator.go
@@ -3,10 +3,10 @@ package data
 import (
 	"context"
 	"fmt"
+	"github.com/lucasjones/reggen"
 
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/pkg/errors"
-	"syreclabs.com/go/faker"
 )
 
 type stringGenerator struct {
@@ -31,15 +31,16 @@ func (generator *stringGenerator) GenerateDataBySchema(ctx context.Context, sche
 	var value Data
 	var err error
 
+	maxLength := 0
+	if schema.MaxLength != nil {
+		maxLength = int(*schema.MaxLength)
+	}
+
 	if len(schema.Enum) > 0 {
 		value = generator.getRandomEnumValue(schema.Enum)
 	} else if schema.Pattern != "" {
-		value, err = generator.generateValueByPattern(schema.Pattern)
+		value, err = generator.generateValueByPattern(schema.Pattern, maxLength)
 	} else if formatGenerator, isSupported := generator.formatGenerators[schema.Format]; isSupported {
-		maxLength := 0
-		if schema.MaxLength != nil {
-			maxLength = int(*schema.MaxLength)
-		}
 		value = formatGenerator(int(schema.MinLength), maxLength)
 	} else {
 		value, err = generator.textGenerator.GenerateDataBySchema(ctx, schema)
@@ -52,8 +53,8 @@ func (generator *stringGenerator) getRandomEnumValue(enum []interface{}) string 
 	return fmt.Sprint(enum[generator.random.Intn(len(enum))])
 }
 
-func (generator *stringGenerator) generateValueByPattern(pattern string) (string, error) {
-	value, err := faker.Regexify(pattern)
+func (generator *stringGenerator) generateValueByPattern(pattern string, maxLength int) (string, error) {
+	g, err := reggen.NewGenerator(pattern)
 	if err != nil {
 		return "", errors.WithStack(&ErrGenerationFailed{
 			GeneratorID: "stringGenerator",
@@ -61,6 +62,9 @@ func (generator *stringGenerator) generateValueByPattern(pattern string) (string
 			Previous:    err,
 		})
 	}
-
+	if maxLength <= 0 {
+		maxLength = defaultMaxLength
+	}
+	value := g.Generate(maxLength)
 	return value, nil
 }
